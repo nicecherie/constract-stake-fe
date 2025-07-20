eaca7695764fe8453e905bcebd2282c9e9a786ea
nicecherie <huangcherie@163.com>
2025-07-20 15:47:07 +0800

    add: 项目初始化

---
+++ app/Header/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Link from 'next/link'
+import { ethers } from 'ethers'
+
+const Header = () => {
+  return (
+    <>
+      <div>Header Page</div>
+    </>
+  )
+}
+export default Header
---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import Header from './Header/page'
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin']
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin']
+})
+
+export const metadata: Metadata = {
+  title: 'MetaNodeStack',
+  description: 'Welcome to invest in this project'
+}
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header />
+        {children}
+      </body>
+    </html>
+  )
+}
